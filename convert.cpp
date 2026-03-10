6797232e51aae56895656a1bdb10320e4e3b61da
Michael L. Collard <collard@uakron.edu>
2026-03-10 16:28:29 -0400

    Move text declartion out of processing hunk

--- convert.cpp
+++ convert.cpp
@@ -19,12 +19,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // text to convert
+    // @concerns input format, argv, std::string, text[out]
+    std::string text(argv[2]);
+
     // convert the string according to the option
     // @concerns input format, argv, std::string
     // @concerns std::string_view, "--upper", "--lower"
     // @concerns std::toupper(), std::tolower(), iteration
-    // @concerns error handling, std::cerr, text[out]
-    std::string text(argv[2]);
+    // @concerns error handling, std::cerr, text
     if (std::string_view(argv[1]) == "--upper") {
 
         for (auto pc = text.begin(); pc != text.end(); ++pc)
